ad80b1185b2a7b44162fe2e8afccf66645dd1765
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-04-08 19:43:36 -0300

    iniciando conteúdo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import React from 'react';
 function App() {
   return (
     <>
-      
+      <header>
+        menu
+      </header>
+        <section>
+          <header className="title">Nome</header>
+          <article className="container-info">
+            <div className="formacao">formação</div>
+            <div className="experiencia">experiencia</div>
+            <div className="qualificacoes">Qualificação</div>
+          </article>
+        </section>
+      <footer>Contato</footer>
     </>
   );
 }
