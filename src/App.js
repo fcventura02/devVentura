28b24eab2b5c4d8a2f29186f9e2c63667060dcf7
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-04-09 16:36:39 -0300

    Trabalhado as rotas, header e o footer.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import MainRoute from './router'
-import { BrowserRouter as Router} from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
+
+
+import './style.css';
 function App() {
   return (
     <Router>
-      <MainRoute/>
+      <header className="cabecalho">
+        <h1>Dev Ventura</h1>
+
+        <div className="menu">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/tcc">TCC</Link></li>
+          </ul>
+        </div>
+      </header>
+      <MainRoute />
+      <footer>
+        <a  target="_blank" rel="noopener noreferrer" href="https://github.com/fcventura02">
+          <h2>gitHub</h2>
+          <img src="https://img.icons8.com/ios-filled/60/000000/github.png"  alt="github"/>
+        </a>
+        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/fcventura02/">
+          <h2>Linkedin</h2>
+          <img src="https://img.icons8.com/ios-filled/50/000000/linkedin-circled.png" alt="Linkedin"/>
+        </a>
+        <a target="_blank" rel="noopener noreferrer"href={`mailto:Filipecvr4@gmail.com`}>
+          <h2>Email</h2>
+          <img src="https://img.icons8.com/ios-filled/50/000000/gmail.png" alt="e-mail"/>
+        </a>
+      </footer>
     </Router>
   );
 }
--- src/router.js
+++ src/router.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
-import SecondPage from './pages/secondPage'
+import Tcc from './pages/tcc'
 
 export default function MainRouter(){
     return(
         <Routes>
                 <Route path='/' element={<Home/>}/>
-                <Route path='/secondpage' element={<SecondPage/>}/>
+                <Route path='tcc' element={<Tcc/>}/>
         </Routes>
     )
 }
