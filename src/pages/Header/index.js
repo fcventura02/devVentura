bf9ae1f5cee65878d12124934581995007af9d08
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2020-04-26 19:22:10 -0300

    resolvendo overflow hidden do botão hamburguer.

--- src/pages/Header/index.js
+++ src/pages/Header/index.js
@@ -3,22 +3,28 @@ import { NavLink } from 'react-router-dom';
 import './style.css';
 
 function App() {
-    const onCheck = () => {
-        let input = document.getElementById('menu__hamburguer')
-        input.attributes.setNamedItem('!checked')
+    const menuClick = () => {
+        let inputChecked = document.getElementById('menu__hamburguer')
+        let link = document.getElementsByClassName('menu__item')
+        let divRoot = document.getElementById('root')
+
+        divRoot.style.overflow = inputChecked.checked ? 'hidden' : '';
+
+        for (let i = 0; link.length > i; i++) {
+            link[i].addEventListener('click', () => {
+                inputChecked.checked = false
+                divRoot.style.overflow = ''
+            })
+        }
     }
 
-    const onLockScroll = () => {
-        let divRoot = document.getElementById('root')
-        //input.style.overflow('hidden')
-        console.log(divRoot.style.overflow = 'hidden')
-    }
+
     return (
         <header className="cabecalho">
             <div className="cabecalho-menu">
                 <h1><NavLink activeClassName='menu__item--active' to="/">Dev Ventura</NavLink></h1>
 
-                <input onClick={() => onLockScroll()} type="checkbox" id="menu__hamburguer" />
+                <input type="checkbox" onClick={() => menuClick()} id="menu__hamburguer" />
                 <label htmlFor="menu__hamburguer">
                     <div className="menu">
                         <div className="menu__hamburguer__item">
@@ -28,8 +34,8 @@ function App() {
 
 
                     <div className="menu__itens">
-                        <NavLink onClick={() => onCheck()} activeClassName='menu__item--active' className="menu__item" to="/">Home</NavLink>
-                        <NavLink onClick={() => onCheck()} activeClassName='menu__item--active' className="menu__item " to="/tcc">TCC</NavLink>
+                        <NavLink activeClassName='menu__item--active' className="menu__item" to="/">Home</NavLink>
+                        <NavLink activeClassName='menu__item--active' className="menu__item " to="/tcc">TCC</NavLink>
                     </div>
                 </label>
             </div>
